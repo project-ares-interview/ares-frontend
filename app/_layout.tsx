aa9663d2bfe77ca78046c20928128f48122d0ba6
HanByeol Jang <j.1star.0726@gmail.com>
2025-09-02 09:41:20 +0900

    feat: 페이지 레이아웃 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,82 +1,35 @@
-import "@/i18n";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { ButtonGroup, Header } from "@rneui/themed";
 import { Stack } from "expo-router";
-import { Suspense, useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { Suspense } from "react";
+import { View } from "react-native";
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from "react-native-safe-area-context";
 
-const supportLanguages = ["en", "ko"];
-
-function StackLayout() {
-  const insets = useSafeAreaInsets();
-
-  return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: {
-          paddingTop: insets.top,
-          paddingBottom: insets.bottom,
-          paddingLeft: insets.left,
-          paddingRight: insets.right,
-        },
-      }}
-    />
-  );
-}
+import Header from "@/components/ui/Header";
+import "@/i18n";
 
 export default function RootLayout() {
-  const { i18n } = useTranslation();
-
-  const changeLanguage = async (language: "en" | "ko") => {
-    i18n.changeLanguage(language);
-    await AsyncStorage.setItem("@app_language", language);
-  };
-
-  const [selectedIndex, setSelectedIndex] = useState(
-    supportLanguages.indexOf(i18n.language),
-  );
-
-  useEffect(() => {
-    if (i18n.language) {
-      const initialIndex = supportLanguages.indexOf(i18n.language);
-      if (initialIndex !== -1) {
-        setSelectedIndex(initialIndex);
-      }
-    }
-  }, [i18n.language]);
+  const insets = useSafeAreaInsets();
 
   return (
     <Suspense fallback={null}>
       <SafeAreaProvider>
-        <Header
-          centerComponent={{
-            text: "React Native",
-            style: {
-              fontSize: 32,
-              fontWeight: "bold",
-              color: "#fff",
-              justifyContent: "center",
-            },
+        <View
+          style={{
+            flex: 1,
+            paddingBottom: insets.bottom,
           }}
-          rightComponent={
-            <ButtonGroup
-              buttons={supportLanguages.map((language) =>
-                language.toUpperCase(),
-              )}
-              selectedIndex={selectedIndex}
-              onPress={(value) => {
-                setSelectedIndex(value);
-                changeLanguage(supportLanguages[value] as "en" | "ko");
-              }}
-            />
-          }
-        />
-        <StackLayout />
+        >
+          <Header />
+
+          <Stack
+            screenOptions={{
+              headerShown: false,
+              animation: "none",
+            }}
+          />
+        </View>
       </SafeAreaProvider>
     </Suspense>
   );
