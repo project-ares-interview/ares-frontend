dddc5cadad8ec6baa115859d24488e98ee421755
HanByeol Jang <j.1star.0726@gmail.com>
2025-09-02 11:00:47 +0900

    feat: useSignIn hook, react-hook-form의 controller를 사용하여 이메일, 비밀번호 검증

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,23 +1,14 @@
 import { Button, Icon, Input, Text } from "@rneui/themed";
-import { Href, Link, useRouter } from "expo-router";
-import React, { useState } from "react";
+import { Href, Link } from "expo-router";
+import React from "react";
+import { Controller } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { useSignIn } from "../hooks/useSignIn";
 
 export default function SignIn() {
   const { t } = useTranslation();
-  const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSignIn = () => {
-    if (!email || !password) {
-      alert("Please enter both email and password.");
-      return;
-    }
-
-    router.replace("/");
-  };
+  const { control, errors, submit } = useSignIn();
 
   return (
     <View style={styles.container}>
@@ -26,32 +17,50 @@ export default function SignIn() {
       </Text>
       <Text style={styles.subtitle}>Sign in to continue</Text>
 
-      <Input
-        placeholder="Email"
-        leftIcon={
-          <Icon name="email" type="material-community" color="#8E8E93" />
-        }
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        keyboardType="email-address"
-        containerStyle={styles.inputContainer}
-        inputStyle={styles.inputText}
+      <Controller
+        control={control}
+        name="email"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Input
+            placeholder="Email"
+            leftIcon={
+              <Icon name="email" type="material-community" color="#8E8E93" />
+            }
+            value={value}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            autoCapitalize="none"
+            keyboardType="email-address"
+            containerStyle={styles.inputContainer}
+            inputStyle={styles.inputText}
+            errorMessage={errors.email?.message}
+          />
+        )}
       />
-      <Input
-        placeholder="Password"
-        leftIcon={
-          <Icon name="lock" type="material-community" color="#8E8E93" />
-        }
-        value={password}
-        onChangeText={setPassword}
-        containerStyle={styles.inputContainer}
-        inputStyle={styles.inputText}
+
+      <Controller
+        control={control}
+        name="password"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Input
+            placeholder="Password"
+            leftIcon={
+              <Icon name="lock" type="material-community" color="#8E8E93" />
+            }
+            value={value}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            containerStyle={styles.inputContainer}
+            inputStyle={styles.inputText}
+            errorMessage={errors.password?.message}
+            secureTextEntry
+          />
+        )}
       />
 
       <Button
         title={t("pages.sign_in.sign_in")}
-        onPress={handleSignIn}
+        onPress={submit}
         containerStyle={styles.signInButtonContainer}
         buttonStyle={styles.signInButton}
         titleStyle={styles.signInButtonText}
