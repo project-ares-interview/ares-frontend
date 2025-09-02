66b776ca967e76b5063db929e76bc8811c4d5f3d
HanByeol Jang <j.1star.0726@gmail.com>
2025-09-02 09:27:05 +0900

    feat: 메인 페이지 작성

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { Link } from "expo-router";
-import { View } from "react-native";
+import Footer from "@/components/ui/Footer";
+import { ScrollView, StyleSheet, View } from "react-native";
 
 import { Text } from "@rneui/themed";
 import { useTranslation } from "react-i18next";
@@ -8,19 +8,25 @@ export default function Index() {
   const { t } = useTranslation();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 8,
-      }}
-    >
-      <Text style={{ fontSize: 64, fontWeight: "bold" }}>
-        {t("welcome")}
-      </Text>
-
-      <Link href="/example">Go to Example</Link>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={{ fontSize: 64, fontWeight: "bold" }}>{t("welcome")}</Text>
+      </View>
+      <Footer />
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 8,
+    minHeight: 1200,
+    paddingVertical: 40
+  },
+});
